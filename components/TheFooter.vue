944b67681d02065354eba9264c8de96f7e92aef9
SMjr <alotoftype@users.noreply.github.com>
2021-04-21 11:36:47 -0400

    initial commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <section class="footer-links">
+      <Navigation />
+    </section>
+    <section>
+      <p>website name copyright {{ getYear }}</p>
+    </section>
+  </footer>
+</template>
+<script>
+export default {
+  computed: {
+    getYear: function () {
+      let d = new Date();
+      return d.getFullYear();
+    },
+  },
+};
+</script>
+<style>
+.footer-links {
+  text-align: center;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="container">
-    <Nuxt/>
+  <div>
+    <TheHeader />
+    <Nuxt />
+    <TheFooter />
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
